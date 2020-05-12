6620696d4830af5b03937dc1cc288fe81b0b21ba
Luca Scotzniovsky <lucascotzniovsky@Lucas-MacBook-Pro.local>
2020-05-12 12:10:04 -0700

    Pushing Individual Tutorial Files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='simple_optimization',
+    version='1',
+    packages=[
+        'simple_optimization',
+    ],
+    install_requires=[
+    ],
+)

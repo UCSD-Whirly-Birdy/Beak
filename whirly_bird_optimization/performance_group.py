95b836303de4f551a8e6dd642b90635a16241b30
Luca Scotzniovsky <lucascotzniovsky@Lucas-MacBook-Pro.local>
2020-05-28 09:17:03 -0700

    Added Performance Group

--- whirly_bird_optimization/performance_group.py
+++ whirly_bird_optimization/performance_group.py
@@ -1,2 +1,37 @@
-from openmdao.api import Group, Problem, IndepVarComp
+from openmdao.api import Group, IndepVarComp
 
+from lsdo_utils.api import PowerCombinationComp
+
+class PerformanceGroup(Group):
+
+    def initialize(self):
+        self.options.declare('shape', types = tuple)
+        self.options.declare('g', types=float)
+        self.options.declare('EMD', types=float)
+        self.options.declare('W0', types=float)
+        self.options.declare('Wb', types=float)
+
+
+    def setup(self):
+
+        shape = self.options['shape']
+        g = self.options['g']
+        EMD = self.options['EMD']
+        W0 = self.options['W0']
+        Wb = self.options['Wb']
+
+        comp = IndepVarComp()
+        comp.add_output('eta') # Propellor Efficiency
+        comp.add_output('LD') # Lift to Drag Ratio
+        self.add_subsystem('inputs_comp', comp, promotes=['*'])
+
+        comp = PowerCombinationComp(
+            shape = shape,
+            out_name = 'Range',
+            coeff = g * EMD * W0 * Wb,
+            powers_dict = dict(
+                eta = 1.,
+                LD = 1.,
+            )
+        )
+        self.add_subsystem('Range_comp', comp, promotes = ['*'])
